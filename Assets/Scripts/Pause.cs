12ac9f4aea8b20b827fc9d79804c6a9b8222e699
Ferrohound <ferrohound@gmail.com>
2015-11-02 09:35:38 -0500

    Spriting

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Pause : MonoBehaviour {
 	bool paused = false;
 	
+	//when pause put the items and menu background active
+	//toggle on and off, paused and unpaused
+	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyUp(KeyCode.Escape))
