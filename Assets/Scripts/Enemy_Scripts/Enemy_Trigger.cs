68147dc1029c95f46045240b7b2b361fd839409d
Ferrohound <ferrohound@gmail.com>
2015-11-03 16:41:04 -0500

    Decoy Script Created

--- Assets/Scripts/Enemy_Scripts/Enemy_Trigger.cs
+++ Assets/Scripts/Enemy_Scripts/Enemy_Trigger.cs
@@ -5,12 +5,14 @@ public class Enemy_Trigger : MonoBehaviour {
 	
 	//give this access to the enemy script
 	Basic_Enemy enemy;
+	Transform target;
 	bool found = false;
 	
 	
 	// Use this for initialization
 	void Start () {
 		enemy = transform.parent.gameObject.GetComponent<Basic_Enemy>();
+		target = enemy.target;
 	}
 	
 	void FixedUpdate(){
@@ -21,7 +23,14 @@ public class Enemy_Trigger : MonoBehaviour {
 		if (other.gameObject.tag == "Player"){
 			Debug.Log("ayylmao");
 			enemy.active = true;
+			enemy.target = target;
 		}
+		if(other.gameObject.tag == "Decoy"){
+			enemy.target = target;
+			enemy.active = false;
+			Destroy(other.gameObject, 3);
+		}
+		
 	}
 	
 	public void OnTriggerExit2D(Collider2D other){
